f5498e916a7573823a6d2af4cd90c49659175b4d
Kien Nguyen <ntk148v@gmail.com>
2017-03-14 11:57:06 +0700

    Init detect pkg

---
+++ lan_sync_controller/detect/discovery.py
@@ -0,0 +1,106 @@
+from __future__ import absolute_import, division, print_function
+import errno
+import logging
+import math
+import socket
+
+import scapy.config
+import scapy.layers.l2
+import scapy.route
+from scapy.all import *
+
+from lan_sync_controller.config_loader import SETTINGS
+
+LOG = logging.getLogger(__name__)
+
+
+def long2net(arg):
+    if (arg <= 0 or arg >= 0xFFFFFFFF):
+        raise ValueError("illegal netmask value", hex(arg))
+    return 32 - int(round(math.log(0xFFFFFFFF - arg, 2)))
+
+
+def to_CIDR_notation(bytes_network, bytes_netmask):
+    network = scapy.utils.ltoa(bytes_network)
+    netmask = long2net(bytes_netmask)
+    net = "%s/%s" % (network, netmask)
+    if netmask < 16:
+        LOG.warning("%s is too big. skipping" % net)
+        return None
+
+    return net
+
+
+def scan_and_get_neighbors(net, interface, timeout=1):
+    LOG.info('arping %s on %s' % (net, interface))
+
+    try:
+        ans, unans = scapy.layers.l2.arping(net, iface=interface,
+                                            timeout=timeout,
+                                            verbose=True)
+        neighbors = []
+        for s, r in ans.res:
+            neighbors.append(r.sprintf('%ARP.psrc%'))
+        return neighbors
+    except socket.error as e:
+        if e.errno == errno.EPERM:
+            LOG.error('%s. Did you run as root?' % (e.strerror))
+        else:
+            raise
+
+
+def scan_udp_port(dst_ip, dst_port, dst_timeout=1):
+    udp_scan_resp = sr1(IP(dst=dst_ip) / UDP(dport=dst_port),
+                        timeout=dst_timeout)
+    if str(type(udp_scan_resp)) == "<type 'NoneType'>":
+        retrans = []
+        for count in range(0, 3):
+            retrans.append(sr1(IP(dst=dst_ip) / UDP(dport=dst_port),
+                               timeout=dst_timeout))
+            for item in retrans:
+                if str(type(item)) != "<type 'NoneType'>":
+                    scan_udp_port(dst_ip, dst_port, dst_timeout)
+            return 'Open|Filtered'
+    elif udp_scan_resp.haslayer(UDP):
+        return 'Open'
+    elif udp_scan_resp.haslayer(ICMP):
+        if int(udp_scan_resp.getlayer(ICMP).type)==3 and \
+            int(udp_scan_resp.getlayer(ICMP).code)==3:
+            return "Closed"
+        elif int(udp_scan_resp.getlayer(ICMP).type) == 3 and \
+            int(udp_scan_resp.getlayer(ICMP).code) in [1,2,9,10,13]:
+            return "Filtered"
+        else:
+            return "CHECK"
+
+
+class NeighborsDetector(object):
+    def __init__(self):
+        self.port = SETTINGS['default-port']
+
+    def get_all_neighbors(self):
+        result = {}
+        for network, netmask, _, interface, address in \
+                scapy.config.conf.route.routes:
+            # skip loopback network and default gw
+            if network == 0 or interface == 'lo' or \
+                    address == '127.0.0.1' or address == '0.0.0.0':
+                continue
+
+            if netmask <= 0 or netmask == 0xFFFFFFFF:
+                continue
+
+            net=to_CIDR_notation(network, netmask)
+
+            if interface != scapy.config.conf.iface:
+                msg=('Skipping %s because scapy currently doesn\'t\
+                       support arping on non-primary network \
+                       interfaces' % net)
+                LOG.warning(msg)
+            if net:
+                result[interface]=scan_and_get_neighbors(net, interface)
+        return result
+
+    def detect_valid_host(self):
+        """Detect valid host, which open a given port"""
+        pass
