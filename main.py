d275a63de3fd5180963027969ab990d7f36c44c2
Kien Nguyen <ntk148v@gmail.com>
2017-05-30 20:32:54 +0700

    Remove print

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from lan_sync_controller.daemon import LANSyncDaemon
 
 PROJECT_ROOT = os.path.abspath(os.path.join(
     os.path.dirname(os.path.realpath(__file__)), '../'))
-print(PROJECT_ROOT)
 if PROJECT_ROOT not in sys.path:
     sys.path.insert(0, PROJECT_ROOT)
 
