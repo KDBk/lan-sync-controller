0593e4e8fad894c34ca3f2aa2fdab96af1b60af3
Kien Nguyen <kiennt@vn.fujitsu.com>
2017-09-21 22:04:45 +0700

    Update Connector class

--- lan_sync_controller/connector.py
+++ lan_sync_controller/connector.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 import pymysql
 
@@ -12,6 +13,7 @@ class MySQLConnector(object):
 
     Will define & declare some methods for special purpose
     """
+
     def __init__(self):
         def _init_mysql_connection():
             try:
@@ -35,5 +37,54 @@ class MySQLConnector(object):
         def __getattr__(self, key):
             return getattr(self.connection, key)
 
-        def get_file_by_name(self):
-            pass
+        def get_file_by_name(self, filename):
+            try:
+                LOG.info('Get file with name %s', filename)
+                # Hardcore table name as files
+                query = """select * from files 
+                        where name=%s
+                        """ % filename
+                cursor = self.cursor()
+                cursor.execute(query)
+                result = self.fetchone()[0]
+                LOG.info('Get file with name %s successfully!', filename)
+            except Exception as e:
+                LOG.exception('Fail to execute query: %s', query)
+                raise e
+
+        def insert_new_file(self, filename, filesize,
+                            last_modified, version=1):
+            try:
+                LOG.info('Insert file with name %s', filename)
+                # Hardcore table name as file_list
+                query = """insert into 
+                        files (name, size, last_modified, version)
+                        values(%s,%s,%s,%s)
+                        """ % (filename, filesize, last_modified, version)
+                cursor = self.cursor()
+                cursor.execute(query)
+                LOG.info('Insert file with name %s successfully!', filename)
+                self.commit()
+            except Exception as e:
+                LOG.exception('Fail to execute query: %s', query)
+                raise e
+
+        def update_file(self, filename, filesize=None, version=1):
+            try:
+                LOG.info('Update file with name %s', filename)
+                last_modified = time.time()
+                # last_modified is time in nanoseconds (Easy to compare)
+                change = 'last_modified=%s' % str(last_modified)
+                if filesize:
+                    change += ' size=%s' % filesize
+                # Hardcore table name as file_list
+                query = """update files set %s 
+                        where name=%s
+                        """ % (change, filename)
+                cursor = self.cursor()
+                cursor.execute(query)
+                LOG.info('Update file with name %s successfully!', filename)
+                self.commit()
+            except Exception as e:
+                LOG.exception('Fail to execute query: %s', query)
+                raise e
