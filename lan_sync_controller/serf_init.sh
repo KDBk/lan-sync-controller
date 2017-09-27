57d5336ac7457acfe03071c42e985c2efa24fc5b
Kien Nguyen <kiennt@vn.fujitsu.com>
2017-09-27 23:58:15 +0700

    Use ip instead of localhost

--- lan_sync_controller/serf_init.sh
+++ lan_sync_controller/serf_init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-serf agent -log-level debug -node=$1 -bind=$2 -event-handler $3 2>&1 > /tmp/serf.log &
+serf agent -log-level debug -node=$1 -bind=$2 -rpc-addr $2:7373 -event-handler $3 2>&1 > /tmp/serf.log &
