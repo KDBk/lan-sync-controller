32c362d2f7ca5003389eb627bc2c83376c5f54c5
Dai Dang Van <daidv@vn.fujitsu.com>
2017-10-01 09:15:05 +0700

    Add try catch for downloading from server

--- lan_sync_controller/daemon.py
+++ lan_sync_controller/daemon.py
@@ -80,13 +80,16 @@ class LANSyncDaemon(base.BaseDaemon):
             for _file in files:
                 file_name = _file['name'].rstrip('/').split('/').pop()
                 filepath = "/".join([watch_dirs[0], file_name])
-                if not os.path.isfile(filepath):
-                    node.swift_connector.download(file_name)
-                else:
-                    local_modified_time = os.path.getmtime(filepath)
-                    if local_modified_time < _file['last_modified']:
-                        # File is out of dated, download it by SwiftConnector
-                        # FUCK. Only get filename not filepath
-                        # _file['name'] = filepath, fuck logic
+                try:
+                    if not os.path.isfile(filepath):
                         node.swift_connector.download(file_name)
+                    else:
+                        local_modified_time = os.path.getmtime(filepath)
+                        if local_modified_time < _file['last_modified']:
+                            # File is out of dated, download it by SwiftConnector
+                            # FUCK. Only get filename not filepath
+                            # _file['name'] = filepath, fuck logic
+                            node.swift_connector.download(file_name)
+                except Exception:
+                    continue  
             time.sleep(10)
